ceb166238eab7d460186d1b0e1c7860e52eda2a1
bambee <daj0909@naver.com>
2024-12-12 07:45:23 +0900

    [feat] 빈조회

--- src/main/java/org/example/nexthw/NexthwApplication.java
+++ src/main/java/org/example/nexthw/NexthwApplication.java
@@ -7,8 +7,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing // @CreatedDate와 @LastModifiedDate 활성화
 @SpringBootApplication
 public class NexthwApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(NexthwApplication.class, args);
-    }
 
+    public static void main(String[] args) {
+//        ApplicationContext ac =
+                SpringApplication.run(NexthwApplication.class, args);
+//        String[] beanNames = ac.getBeanDefinitionNames();
+//        for (String beanName : beanNames) {
+//            Object bean = ac.getBean(beanName);
+//            System.out.println("bean = " + bean);
+//            System.out.println("beanNames = " + beanNames);
+//        }
+
+    }
 }
