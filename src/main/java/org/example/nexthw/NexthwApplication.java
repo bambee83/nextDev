1aec02a260366548a3833b3a7085e72be331da01
bambee <daj0909@naver.com>
2024-12-12 09:35:41 +0900

    [feat] 빈조회

--- src/main/java/org/example/nexthw/NexthwApplication.java
+++ src/main/java/org/example/nexthw/NexthwApplication.java
@@ -2,6 +2,7 @@ package org.example.nexthw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // @CreatedDate와 @LastModifiedDate 활성화
@@ -9,14 +10,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class NexthwApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext ac =
+        ApplicationContext ac =
                 SpringApplication.run(NexthwApplication.class, args);
-//        String[] beanNames = ac.getBeanDefinitionNames();
-//        for (String beanName : beanNames) {
-//            Object bean = ac.getBean(beanName);
-//            System.out.println("bean = " + bean);
-//            System.out.println("beanNames = " + beanNames);
-//        }
+
+        /*"모든 빈 출력하기"*/
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name=" + beanDefinitionName + " object=" +
+                    bean);
+        }
+
+        /*"애플리케이션 빈 출력하기"*/
+        // Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+        // Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
 
     }
 }
