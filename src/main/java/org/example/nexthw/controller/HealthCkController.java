0942f629cbe4b9351135365f8c7b3f4fe9412bc7
bambee <daj0909@naver.com>
2024-12-15 17:58:56 +0900

    [feat] Trace test

--- src/main/java/org/example/nexthw/controller/HealthCkController.java
+++ src/main/java/org/example/nexthw/controller/HealthCkController.java
@@ -1,23 +1,15 @@
 package org.example.nexthw.controller;
 
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@RestController
+@Slf4j @RestController
 public class HealthCkController {
-//    @Value("${spring.profiles.active}")
-//    String profilesActive;
 
-    // MultiProfile test
+    // healthCk
     @GetMapping("/healthCk")
     public String healthCk() {
-//        log.info("Active Profile: {}", profilesActive);
-//        return String.format("Active Profile: %s", profilesActive);
         return "OK";
     }
 
@@ -30,6 +22,8 @@ public class HealthCkController {
         log.warn("WARN!!");
         log.error("ERROR!!");
     }
+
+
 
 
 }
---
+++ src/test/java/org/example/nexthw/utils/TraceV2Test.java
@@ -0,0 +1,26 @@
+package org.example.nexthw.utils;
+
+import org.junit.jupiter.api.Test;
+
+class TraceV2Test {
+
+    @Test
+    void begin_end() {
+        //given
+        TraceV2 trace = new TraceV2();
+        TraceStatus status1 = trace.begin("begin");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "begin2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception() {
+        TraceV2 trace = new TraceV2();
+        TraceStatus status1 = trace.begin("begin");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "begin2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
