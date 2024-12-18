0abc20774aeb4ce9c2bc3c7f81f1df591eeabfb7
bambee <daj0909@naver.com>
2024-12-18 17:03:05 +0900

    [feat]

--- src/main/java/org/example/nexthw/controller/HealthCkController.java
+++ src/main/java/org/example/nexthw/controller/HealthCkController.java
@@ -16,9 +16,6 @@ public class HealthCkController {
     // healthCk
     @GetMapping("/healthCk")
     public String healthCk() throws InterruptedException {
-        log.info("hello - start");
-        Thread.sleep(1000);
-        log.info("hello - end");
         return "OK";
     }
 
