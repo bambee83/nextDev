ba19fc20c42c97c51b829b5151e0c81453bf9949
bambee <daj0909@naver.com>
2024-12-30 17:13:00 +0900

    hotfix. interceptor

--- module-api/src/main/java/org/example/conf/WebConfig.java
+++ module-api/src/main/java/org/example/conf/WebConfig.java
@@ -1,7 +1,6 @@
 package org.example.conf;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
