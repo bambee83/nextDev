b904c1a8408b41a73a61714cc31711f95ec5e920
bambee <daj0909@naver.com>
2025-01-07 01:00:33 +0900

    feat.  Feign

--- module-core/src/main/java/org/example/utils/FeignClientDecoder.java
+++ module-core/src/main/java/org/example/utils/FeignClientDecoder.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.Util;
 import feign.codec.Decoder;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 
+@Slf4j
 public class FeignClientDecoder implements Decoder {
 
     @Override
     public Object decode(Response response, Type type) throws IOException {
         // 응답 본문을 객체로 변환하는 로직
-        String body = Util.toString(response.body().asReader());
-        return new ObjectMapper().readValue(body, (Class<?>) type);
+        String body = Util.toString(response.body().asReader());  // 응답 본문을 문자열로 읽기
+        log.debug("Response Body: {}", body);  // 응답 본문 로그
+        return new ObjectMapper().readValue(body, (Class<?>) type); // JSON 문자열을 DTO로 변환
     }
 }
--- module-service/src/main/java/org/example/dto/FeignClientResponseDto.java
+++ module-service/src/main/java/org/example/dto/FeignClientResponseDto.java
@@ -2,11 +2,9 @@ package org.example.dto;
 
 import lombok.Getter;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 public class FeignClientResponseDto {
-    private Integer userId;
+    private Long userId;
     private Long id;
     private String title;
     private String body;
