e7a833517e9a9744fcac4d8774bf4e2304c918e8
bambee <daj0909@naver.com>
2025-01-15 21:38:27 +0900

    feat.  리펙

--- module-service/src/main/java/org/example/dto/BoardRequestDto.java
+++ module-service/src/main/java/org/example/dto/BoardRequestDto.java
@@ -4,6 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/*
+ * 도메인을 직접적으로 다루는 것은 위험 할 수 있다.
+ * Jpa 의 영속성 컨텍스트 때문에 혹시나 같은 트렌젝션 내에서 엔티티 수정시
+ * 트렌젝션이 끝날때 그대로 DB 에 커밋을 찍을 수도 있다.
+ * 따라서 DB 에서 가져온 후에는 따로 Object 에 넣어야 한다.
+ */
 import javax.validation.constraints.NotBlank;
 
 @Schema(description = "내용 요청 정보")
