1f62dfa810b10b6ae79a460c653dabbbf8ff1b1a
bambee <daj0909@naver.com>
2025-02-04 15:17:46 +0900

    feat.  리펙

--- module-api/src/main/java/org/example/ModuleApiApplication.java
+++ module-api/src/main/java/org/example/ModuleApiApplication.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.example")
 //@PropertySource("classpath:/src/main/resources/properties/application-local.properties")
 public class ModuleApiApplication {
 
